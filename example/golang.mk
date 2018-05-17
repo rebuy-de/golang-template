d8c5f85d555a998058fc632784cc8cb68766ef9c
Sven Walter <s.walter@rebuy.com>
2018-05-17 16:52:28 +0200

    Release 2.0.3

--- example/golang.mk
+++ example/golang.mk
@@ -1,5 +1,5 @@
 # Source: https://github.com/rebuy-de/golang-template
-# Version: 2.0.3-snapshot
+# Version: 2.0.3
 # Dependencies:
 # * dep (https://github.com/golang/dep)
 # * gocov (https://github.com/axw/gocov)
